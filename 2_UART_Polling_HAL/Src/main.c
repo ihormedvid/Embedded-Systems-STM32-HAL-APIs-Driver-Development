7b4f89319af51ac900f887aebe65af687d35959a
ihormedvid <igormedvid89@gmail.com>
2024-07-15 10:37:01 +0300

    added 2 uart polling

--- 2_UART_Polling_HAL/Src/main.c
+++ 2_UART_Polling_HAL/Src/main.c
@@ -1,69 +1,64 @@
 #include "stm32f4xx_hal.h"
 
 
-#define BTN_PORT	GPIOC
-#define BTN_PIN		GPIO_PIN_13
-#define LED_PORT	GPIOA
-#define LED_PIN		GPIO_PIN_5
-
-// BTN = PC13,	BUS = AHB1EN bit0
-// LED = PA5,	BUS = AHB1EM bit2
+UART_HandleTypeDef huart2;
 
 
-void pc13_btn_init(void);
-void pa5_led_init(void);
+void uart_init(void);
 
-uint8_t buttonStatus;
+char message[20] = "Hello from STM32\r\n";
 
 int main() {
 
 	HAL_Init();
-	pc13_btn_init();
-	pa5_led_init();
+	uart_init();
 
 
 	while(1) {
 
-		buttonStatus = HAL_GPIO_ReadPin(BTN_PORT,BTN_PIN);
-		HAL_GPIO_WritePin(LED_PORT, LED_PIN, buttonStatus);
+		HAL_UART_Transmit(&huart2, (uint8_t *) message, 20, 100);
+		HAL_Delay(10);
 	}
 }
 
-void pc13_btn_init(void) {
 
-
-	/* Configure the PC13 as input pins */
-	GPIO_InitTypeDef	GPIO_InitStruct = {0};
-
-	/* Enable clock access to GPIO C */
-	__HAL_RCC_GPIOC_CLK_ENABLE();
-	GPIO_InitStruct.Pin   = BTN_PIN;
-	GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull  = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-	/* initialize  the GPIO to be ready for use */
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-}
-
-
-void pa5_led_init(void) {
-
-	/* Configure the PA 5 output pin */
-	GPIO_InitTypeDef	GPIO_InitStruct = {0};
-
-	/* Enable clock access to GPIO A */
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	GPIO_InitStruct.Pin   = LED_PIN;
-	GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull  = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-	/* initialize  the GPIO to be ready for use */
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
 
 void SysTick_Handler(void){
 
 	HAL_IncTick();
+}
+
+
+void uart_init(void) {
+
+
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	// Enable UART pins clock access
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	// Enable UART module clock access
+	__HAL_RCC_USART2_CLK_ENABLE();
+
+	// Configure pint to act as as alternate func pins (UART)
+
+	GPIO_InitStruct.Pin		  = GPIO_PIN_2 | GPIO_PIN_3;
+	GPIO_InitStruct.Mode 	  = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+	GPIO_InitStruct.Pull 	  = GPIO_NOPULL;
+	GPIO_InitStruct.Speed 	  = GPIO_SPEED_FREQ_VERY_HIGH;
+
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	// Configure UART module
+	huart2.Instance 			= USART2;
+	huart2.Init.BaudRate		= 115200;
+	huart2.Init.WordLength 		= UART_WORDLENGTH_8B;
+	huart2.Init.StopBits		= UART_STOPBITS_1;
+	huart2.Init.Parity 			= UART_PARITY_NONE;
+	huart2.Init.Mode 			= UART_MODE_TX;
+	huart2.Init.HwFlowCtl		= UART_HWCONTROL_NONE;
+	huart2.Init.OverSampling	= UART_OVERSAMPLING_16;
+
+	HAL_UART_Init(&huart2);
 }
