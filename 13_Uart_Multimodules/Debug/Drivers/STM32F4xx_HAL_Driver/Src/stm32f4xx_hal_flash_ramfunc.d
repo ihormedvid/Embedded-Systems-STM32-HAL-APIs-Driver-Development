ff2cd25b94edd1e81065c30d792f80a432459d88
ihormedvid <igormedvid89@gmail.com>
2024-07-24 13:09:12 +0300

    add 13 UART Multi Modules

---
+++ 13_Uart_Multimodules/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.d
@@ -0,0 +1,56 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/core_cm4.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/core_cm4.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/cmsis_version.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/cmsis_compiler.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Include/mpu_armv7.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+D:/projects/stm_hal/13_Uart_Multimodules/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h:
