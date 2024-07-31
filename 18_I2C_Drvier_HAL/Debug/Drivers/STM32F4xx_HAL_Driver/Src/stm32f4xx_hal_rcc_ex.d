d96734872e1eaa47681954548bbd095cd9c2a07e
ihormedvid <igormedvid89@gmail.com>
2024-07-31 19:37:50 +0300

    add 18 I2C driver HAL

---
+++ 18_I2C_Drvier_HAL/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.d
@@ -0,0 +1,70 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/core_cm4.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/core_cm4.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/cmsis_version.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/cmsis_compiler.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/cmsis_gcc.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Include/mpu_armv7.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+D:/projects/stm_hal/18_I2C_Drvier_HAL/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_usart.h:
